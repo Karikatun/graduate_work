cf6de6582f449435ba78b29d31e611b9e9957ab7
Anton Shevchenko <41957518+Karikatun@users.noreply.github.com>
2018-12-16 01:35:15 +0300

    Модальное окно при посещении сайта более 60 секунд

---
+++ src/parts/minuteModalWindow.js
@@ -0,0 +1,16 @@
+function minuteModalWindow() {
+    let popupConsult = document.querySelector('.popup-consultation'),
+        popupDesign = document.querySelector('.popup-design'),
+        popupGift = document.querySelector('.popup-gift');    
+
+    let timerId = setTimeout(() => {
+        if (popupConsult.style.display == 'block' ||  popupGift.style.display == 'block' || popupDesign.style.display == 'block') {
+            clearTimeout(timerId);
+        } else {
+            popupConsult.style.display = 'block';
+        }
+    }, 5000);
+
+}
+
+module.exports = minuteModalWindow;
