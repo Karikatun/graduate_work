598780735228ceb66ba18c23b572081969f9f1c6
Anton Shevchenko <41957518+Karikatun@users.noreply.github.com>
2018-12-16 15:14:53 +0300

    Add files via upload

--- src/parts/minuteModalWindow.js
+++ src/parts/minuteModalWindow.js
@@ -1,10 +1,11 @@
 function minuteModalWindow() {
     let popupConsult = document.querySelector('.popup-consultation'),
+        popupForm = popupConsult.querySelector('#popup-form'),
         popupDesign = document.querySelector('.popup-design'),
         popupGift = document.querySelector('.popup-gift');    
 
     let timerId = setTimeout(() => {
-        if (popupConsult.style.display == 'block' ||  popupGift.style.display == 'block' || popupDesign.style.display == 'block') {
+        if (popupConsult.style.display == 'block' ||  popupGift.style.display == 'block' || popupDesign.style.display == 'block' || popupForm.style.display == 'block') {
             clearTimeout(timerId);
         } else {
             popupConsult.style.display = 'block';
