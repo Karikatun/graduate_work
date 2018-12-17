460d15ec71f8c26b4d0f99cd9634e75d5c0862fa
Anton Shevchenko <41957518+Karikatun@users.noreply.github.com>
2018-12-17 14:17:16 +0300

    Подгрузка блоков

--- src/parts/stylesBlock.js
+++ src/parts/stylesBlock.js
@@ -1,15 +1,15 @@
 function stylesBlock() {
-    let stylesBlock = document.querySelectorAll('.styles-block'),
+    let stylesBlock = document.querySelectorAll('.hidden-block'),
         buttonStyles = document.querySelector('.button-styles');
 
-    // window.addEventListener('click', (e) => {
-    //     if (e.target && e.target.classList.contains('button-styles')){
-    //         for (let i = 0; i < stylesBlock.length; i++){
-    //             stylesBlock[i].style.display = 'block';
-    //         }
-    //         buttonStyles.style.display = 'none';
-    //     }
-    // });
+    window.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('button-styles')){
+            for (let i = 0; i < stylesBlock.length; i++){
+                stylesBlock[i].setAttribute('style', 'display:block !important');
+            }
+            buttonStyles.style.display = 'none';
+        }
+    });
 }
 
 module.exports = stylesBlock;
