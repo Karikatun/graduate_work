f699eeee16eb45f0a1d7e090caf4c61f6a564743
Anton Shevchenko <41957518+Karikatun@users.noreply.github.com>
2018-12-19 23:37:29 +0300

    картинки по наведению

--- src/parts/sizesWrapper.js
+++ src/parts/sizesWrapper.js
@@ -4,23 +4,29 @@ function sizesWrapper() {
         arrPicsDef = ['img/sizes-1.png', 'img/sizes-2.png', 'img/sizes-3.png', 'img/sizes-4.png'],
         arrPics = ['img/sizes-1-1.png', 'img/sizes-2-1.png', 'img/sizes-3-1.png', 'img/sizes-4-1.png'];
 
-    for (let i = 0; i < sizesBlock.length; i++) {
-        sizesBlock[i].addEventListener('mouseover', function (e) {
-            e.target.src = arrPics[i];
-            let p = sizesBlock[i].querySelectorAll('p');
-            for (let i = 0; i < 3; i++) {
-                p[i].style.display = 'none';
-            }
-        });
-        sizesBlock[i].addEventListener('mouseout', function (e) {
-            let p = sizesBlock[i].querySelectorAll('p');
-            for (let i = 0; i < 3; i++) {
-                p[i].style.display = 'block';
-            }
-            e.target.src = arrPicsDef[i];
+    // for (let i = 0; i < sizesBlock.length; i++) {
+    //     sizesBlock[i].addEventListener('mouseover', function (e) {
+    //         e.target.src = arrPics[i];
+    //         let p = sizesBlock[i].querySelectorAll('p'),
+    //             img = sizesBlock[i].querySelector('img');
+    //         img.style.cssText = 'z-index: 10';
+    //         for (let i = 0; i < 3; i++) {
+    //             p[i].style.cssText = 'z-index: -1';
+    //         }
+    //     });
+    //     sizesBlock[i].addEventListener('mouseout', function (e) {
+    //         let p = sizesBlock[i].querySelectorAll('p'),
+    //             img = sizesBlock[i].querySelector('img');
+    //         img.style.cssText = 'z-index: -1';
+    //         for (let i = 0; i < 3; i++) {
+    //             p[i].style.cssText = 'z-index: 10';
+    //         }
+    //         e.target.src = arrPicsDef[i];
 
-        });
-    }
+    //     });
+    // }
+
+    
 }
 
 module.exports = sizesWrapper;
