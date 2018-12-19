784bd4d53d8dcc89a02d497991b9254131ba1f63
Anton Shevchenko <41957518+Karikatun@users.noreply.github.com>
2018-12-19 17:19:47 +0300

    размеры скрытых блоков

--- src/parts/stylesBlock.js
+++ src/parts/stylesBlock.js
@@ -5,7 +5,16 @@ function stylesBlock() {
     window.addEventListener('click', (e) => {
         if (e.target && e.target.classList.contains('button-styles')){
             for (let i = 0; i < stylesBlock.length; i++){
-                stylesBlock[i].setAttribute('style', 'display:block !important');
+                let img = stylesBlock[i].querySelector('img'),
+                    block = stylesBlock[i].querySelector('.styles-block');                
+                stylesBlock[i].setAttribute('style', 'display:inline-block !important');
+                stylesBlock[i].style.width = '292.5px';
+                stylesBlock[i].style.heght = '373px';
+                stylesBlock[i].style.paddingLeft = '15px';
+                stylesBlock[i].style.paddingRight = '15px';
+                block.style.position = 'relative';
+                img.style.heght = '214.0px';
+                img.style.width = '153px';
             }
             buttonStyles.style.display = 'none';
         }
